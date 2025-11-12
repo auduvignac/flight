4e1b9b48069c32f51f793fdda71bdf69011dd727
@JeanMarcFauvel <jean-marc.fauvel@wanadoo.fr>
2025-11-12 20:08:22 +0100

    WIP 251112:2008

--- src/main/scala/com/emiasd/flight/Main.scala
+++ src/main/scala/com/emiasd/flight/Main.scala
@@ -350,8 +350,16 @@ object Main {
 
       // Option : exécuter une seule étape si argument fourni
       logger.info(s"Args = ${args.toList}")
-      val stage = args.headOption.getOrElse("all").toLowerCase
+
+
+      //val stage = args.headOption.getOrElse("all").toLowerCase
+      val stage = args.headOption.getOrElse {
+        logger.error("Aucun stage fourni (attendu: bronze | silver | gold | all)")
+        sys.exit(2)
+      }.replaceFirst("^--stage=", "").toLowerCase
+
       logger.info(s"Stage demandé = $stage")
+
       stage match {
         case "bronze" =>
           runBronze(spark, paths)
